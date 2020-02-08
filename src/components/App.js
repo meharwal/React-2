fad46024c8ee19d0b4f6b3d1df709bd41f11ac06
HrithikMittal <adhikanshmittal@gmail.com>
2020-02-08 15:49:54 +0530

    add bootsrap form

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,34 @@ import React, { Component } from "react";
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hey I am Adhikansh Mittal</h1>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-sm-6 col-sm-offset-3">
+            <form>
+              <div className="form-group">
+                <input
+                  type="text"
+                  name="title"
+                  className="form-control no-border"
+                  placeholder="Title..."
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="text"
+                  name="body"
+                  className="form-control no-border"
+                  placeholder="Body..."
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <button className="btn btn-primary col-sm-12">Save</button>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     );
   }
