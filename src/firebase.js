e41d953d41652c0b2e18fbf4be81dec7af3ec7e6
HrithikMittal <adhikanshmittal@gmail.com>
2020-02-08 15:28:53 +0530

    add firebase

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import * as firebase from "firebase";
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyD39U37UxmUATQ0wAV2ulFZbyIys1JU5vY",
+  authDomain: "diary-c602f.firebaseapp.com",
+  databaseURL: "https://diary-c602f.firebaseio.com",
+  projectId: "diary-c602f",
+  storageBucket: "diary-c602f.appspot.com",
+  messagingSenderId: "314029576798",
+  appId: "1:314029576798:web:613287d52b60fdb6ad9acf",
+  measurementId: "G-QB4H08Y33R"
+};
+firebase.initializeApp(config);
+
+export const database = firebase.database().ref("/notes");
