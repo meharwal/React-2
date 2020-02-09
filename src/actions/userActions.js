04d47b436972132c99abb93ef7bc5d00cadc7b59
HrithikMittal <adhikanshmittal@gmail.com>
2020-02-09 08:45:26 +0530

    loading component

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,12 +1,24 @@
 import { auth, googleProvider, twitterProvider } from "../firebase";
-import { GET_USER } from "../actionTypes";
+import { GET_USER, USER_STATUS } from "../actionTypes";
 
 export function getUser() {
   return dispatch => {
+    // SHOW LOADING STATUS BEFORE GETTING USE TO TRUE
+    dispatch({
+      type: USER_STATUS,
+      payload: true
+    });
+
     auth.onAuthStateChanged(user => {
       dispatch({
         type: GET_USER,
         payload: user
+      });
+
+      // SHOW LOADING STATUS TO FALSE
+      dispatch({
+        type: USER_STATUS,
+        payload: false
       });
     });
   };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,13 @@ import { combineReducers } from "redux";
 
 import notesReducer from "./notesReducer";
 import userReducer from "./userReducer";
+import loadingReducer from "./loadingReducer";
+import userReducer from "./userReducer";
 
 const rootReducer = combineReducers({
   notes: notesReducer,
-  user: userReducer
+  user: userReducer,
+  loading: loadingReducer
 });
 
 export default rootReducer;
