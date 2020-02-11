10af414f06aa974b30598d440b0a14a7255000a7
HrithikMittal <adhikanshmittal@gmail.com>
2020-02-11 06:24:05 +0530

    change the os

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,7 +3,6 @@ import { combineReducers } from "redux";
 import notesReducer from "./notesReducer";
 import userReducer from "./userReducer";
 import loadingReducer from "./loadingReducer";
-import userReducer from "./userReducer";
 
 const rootReducer = combineReducers({
   notes: notesReducer,
--- src/reducers/loadingReducer.js
+++ src/reducers/loadingReducer.js
@@ -2,7 +2,7 @@ import { NOTES_STATUS, USER_STATUS } from "../actionTypes";
 
 export default function(state = {}, action) {
   switch (action.type) {
-    case NOTE_STATUS:
+    case NOTES_STATUS:
       return { ...state, notes: action.payload };
     case USER_STATUS:
       return { ...state, user: action.payload };
